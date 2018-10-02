472bbb7ccd2a4157238f56a6e161ba7ca8b76a47
ChuanQin <callforsky@gmail.com>
2018-10-01 23:14:20 -0400

    Able to switch between valid users and guest!

--- src/components/LogStatus.js
+++ src/components/LogStatus.js
@@ -2,10 +2,15 @@ import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { Header, Button, Popup, Grid, Image, Modal } from 'semantic-ui-react'
 import { withRouter } from 'react-router-dom'
+import { setAuthedUser } from '../actions/authedUser';
 
 class LogStatus extends Component {
-	jumpToLoginPage = () => {
-		// logout from current authentication. Change authedUser to null
+	jumpToLoginPage = (e, authedUser) => {
+		// logout from current authentication. If already a guest, simply jump to login page
+		const { dispatch } = this.props
+		if (authedUser !== 'guest') {
+			dispatch(setAuthedUser('guest'))
+		}
 
 		// jump to login page
 		this.props.history.push('/login');
@@ -13,13 +18,13 @@ class LogStatus extends Component {
 
 	render() {
 		const { userInfo, authedUser } = this.props
-		console.log(authedUser)
+
 		return (
 			<div className='right'>
 				<span>Hello, {userInfo.name}</span>
 				{authedUser !== 'guest'
-					? <Button basic compact onClick={this.jumpToLoginPage}>Logout</Button>
-					: <Button basic compact onClick={this.jumpToLoginPage}>Log In</Button>
+					? <Button basic compact onClick={e => this.jumpToLoginPage(e, authedUser)}>Logout</Button>
+					: <Button basic compact onClick={e => this.jumpToLoginPage(e, authedUser)}>Log In</Button>
 				}
 			</div>
 		)
