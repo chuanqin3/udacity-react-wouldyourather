81a6503b4343f1da294b8f46972636ab89a12cc6
ChuanQin <callforsky@gmail.com>
2018-10-07 11:05:29 -0400

    Added Link to 404 Error Page

--- src/components/NoMatch.js
+++ src/components/NoMatch.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
+import { Image } from 'semantic-ui-react'
 
 const NoMatch = (props) => (
-  <div className='center'>
-    <img src="https://www.lifewire.com/thmb/qLv10Pgd30kCy7OxXacwOWKxZ8M=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/shutterstock_325494917-5a68d8403418c600190a3e1f.jpg" alt="sorry!" width="400" height="300" />
+  <Link to={`/`} className='center'>
+    <Image
+      src="https://www.lifewire.com/thmb/qLv10Pgd30kCy7OxXacwOWKxZ8M=/768x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/shutterstock_325494917-5a68d8403418c600190a3e1f.jpg"
+      alt="sorry!"
+      width="400"
+      height="300"
+      centered
+    />
     <p>Sorry! This page doesn't exist. Did you type the correct url?</p>
-  </div>
+  </Link>
 );
 export default NoMatch;
